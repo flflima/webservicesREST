45ab0d6b54e4728248136a6839d71cb2044dcde3
Felipe Lima <flf.felipe@gmail.com>
2016-06-16 19:08:58 -0300

    Commmit de CRUD com REST.

---
+++ crudClienteREST/src/br/com/dev/entidades/Cliente.java
@@ -0,0 +1,35 @@
+package br.com.dev.entidades;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Cliente {
+	private long id;
+	private String nome;
+	private int idade;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
