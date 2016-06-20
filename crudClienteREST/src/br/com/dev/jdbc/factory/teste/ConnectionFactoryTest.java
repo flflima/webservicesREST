cc400f5bdf21779bb199b5e503a86e29b5ed18ba
Felipe Lima <flf.felipe@gmail.com>
2016-06-20 19:03:08 -0300

    Criação da ConnectionFactoryTest.

---
+++ crudClienteREST/src/br/com/dev/jdbc/factory/teste/ConnectionFactoryTest.java
@@ -0,0 +1,20 @@
+package br.com.dev.jdbc.factory.teste;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import org.junit.Test;
+
+import br.com.dev.jdbc.factory.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+
+	@Test
+	public void testGetConnection() {
+		Connection connection = ConnectionFactory.getConnection();
+		
+		assertNotNull(connection);;
+	}
+
+}
