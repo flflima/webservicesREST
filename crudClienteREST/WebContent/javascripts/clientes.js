69a92af466ba510c0777ed4ddda1f8ae22325024
Felipe Lima <flf.felipe@gmail.com>
2016-07-01 19:08:05 -0300

    Alterações cliente.js.

--- crudClienteREST/WebContent/javascripts/clientes.js
+++ crudClienteREST/WebContent/javascripts/clientes.js
@@ -3,11 +3,18 @@
  */
 $(function() {
 	
-	$.getJSON("http://localhost:8080/crudCliente/crud/clientes", function(data) {
-
-		$.each(data, function(i, item)
-		{
-			$("#listaClientes").append("<tr><td>" + item.nome + "</td><td>" + item.idade + "</td></tr>");
+	function carregarClientes() {
+		$.getJSON("http://localhost:8080/crudCliente/crud/clientes", function(data) {
+			
+			$.each(data, function(i, item)
+			{
+				var dados = "<td>" + item.nome + "</td><td>" + item.idade + "</td>";
+				console.log("http://localhost:8080/crudCliente/crud/clientes/" + item.id);
+				var linkDetalhar = "<td><a href='http://localhost:8080/crudCliente/detalhar_cliente.html?id=" + item.id + "'>Detalhar</a></td>";
+				$("#listaClientes").append("<tr>" + dados + linkDetalhar + "</tr>");
+			});
 		});
-	});
+	}
+	
+	$(window).load(carregarClientes);
 });
